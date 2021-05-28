bd6c942bc3ddfcd455d25e412d3c5019c88cdb99
CrispenGari <Crispengari@gmail.com>
2021-05-28 14:34:20 +0200

    sessions

---
+++ 06_Sessions/app/main.py
@@ -0,0 +1,40 @@
+from datetime import timedelta
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+app.secret_key = "abcd"
+# setting the lifetime of the session
+app.permanent_session_lifetime = timedelta(days=7)
+
+@app.route('/', methods=["GET", "POST"])
+def auth():
+    error = ""
+    if request.method == "POST":
+        if request.form["username"] == 'admin' and  request.form["password"] == '12345':
+           # Set the session permenant to true
+           session.permanent = True
+           session["user"] = {
+               "username": request.form["username"],
+               "password": request.form["password"]
+           }
+           return redirect(url_for('home_page'))
+        else:
+            error="Invalid username or password."
+    else:
+        error = "Unknown authentication error."
+    return render_template('index.html', error=error)
+
+@app.route('/home')
+def home_page():
+    if session["user"]:
+        return render_template('home.html', user=session["user"])
+    else:
+        return redirect(url_for('auth'))
+
+@app.route('/logout')
+def logout():
+    session["user"] = None
+    return redirect(url_for('auth'))
+
+if __name__ == "__main__":
+    app.run(debug=True) # allow hot reloading
