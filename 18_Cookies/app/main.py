4235d80121ddb00385dc4d9c2d82763e39e2fe19
CrispenGari <Crispengari@gmail.com>
2021-06-05 23:17:14 +0200

    cookies

---
+++ 18_Cookies/app/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask.globals import request
+from flask.helpers import make_response
+
+app = Flask(__name__)
+app.config["ENV"] = "development"
+
+@app.route('/cookies')
+def home():
+    res = make_response(render_template("index.html"))
+    res.set_cookie("username", "root")
+    cookies = request.cookies
+    print(cookies)
+    return res
+
+if __name__ == "__main__":
+    app.run(debug=True)
